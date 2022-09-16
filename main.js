9c3374ab74f667e3a34cadbc82d1934b26e317f1
Jiheng Nie <Niejiheng@gmail.com>
2022-09-16 23:47:31 +0000

    Updated some event listener functions

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var $addEntryButton = document.querySelector('.new-entry-button');
 var $weekDaySpacingDiv = document.querySelector('.weekday-spacing');
 var $tableHeader = document.querySelector('.table-header');
 var $tableSelector = document.querySelector('tbody');
+var $inputArray = document.querySelectorAll('.input');
 
 // event listeners
 $modal.addEventListener('submit', handleModalClick);
@@ -18,17 +19,37 @@ function addEntryButton(event) {
 function handleModalClick(event) {
   event.preventDefault();
   $modal.className = 'hidden';
+  var userInputData = {
+    time: $inputArray[1].value,
+    description: $inputArray[2].value
+  };
+  data[$inputArray[0].value].push(userInputData);
+
+  dataModel.view = $inputArray[0].value;
+  dataModel.view = capitalizeFirstLetter(dataModel.view);
+  $tableHeader.textContent = 'Scheduled Events for ' + dataModel.view;
+  while ($tableSelector.children.length) {
+    $tableSelector.removeChild($tableSelector.firstChild);
+  }
+  createListEntries(data, dataModel.view);
 }
 
 function scheduleUpdate(event) {
+  if (event.target.tagName !== 'P') {
+    return;
+  }
   $tableHeader.textContent = 'Scheduled Events for ' + event.target.textContent;
-  dataModel.view = event.target.textContent.toLowerCase();
-  for (var i = 0; i < $tableSelector.children.length; i++) {
-    $tableSelector.removeChild($tableSelector.children[i]);
+  dataModel.view = event.target.closest('.day').textContent.toLowerCase();
+
+  while ($tableSelector.children.length) {
+    $tableSelector.removeChild($tableSelector.firstChild);
   }
-  if (event.target.textContent.toLowerCase() === dataModel.view) {
-    createListEntries(data, event.target.textContent.toLowerCase());
-  }
+
+  createListEntries(data, event.target.textContent.toLowerCase());
+  // console.log(event.target.textContent);
+  // console.log(event.target);
+  // console.log(data[event.target.textContent.toLowerCase()]);
+  // console.log(data);
 }
 
 // other functions
@@ -50,21 +71,44 @@ function generateDomTree(tagName, attributes, children = []) {
 // Making a DOM Tree
 
 function createListEntries(object, dayOfWeek) {
+  dayOfWeek = dayOfWeek.toLowerCase();
+  var entries;
+  //   if (object[dayOfWeek]) {
+  //     for (var i = 0; i < object[dayOfWeek].length; i++) {
+  //       entries = generateDomTree('tr', {}, [
+  //         generateDomTree('td', { textContent: object[dayOfWeek][i].time }),
+  //         generateDomTree('td', { textContent: object[dayOfWeek][i].description })
+  //       ]);
+  //       $tableSelector.appendChild(entries);
+  //     }
+  //   } else {
+  //     entries = generateDomTree('tr', {}, [
+  //       generateDomTree('td', { textContent: 'No time' }),
+  //       generateDomTree('td', { textContent: 'No plans made yet' })
+  //     ]);
+  //     $tableSelector.appendChild(entries);
+  //   }
+  // }
+
   for (var i = 0; i < object[dayOfWeek].length; i++) {
-    if (object[dayOfWeek][i].time) {
-      var entries = generateDomTree('tr', {}, [
+    if ($tableSelector.textContent) {
+      entries = generateDomTree('tr', {}, [
         generateDomTree('td', { textContent: object[dayOfWeek][i].time }),
         generateDomTree('td', { textContent: object[dayOfWeek][i].description })
       ]);
+      $tableSelector.appendChild(entries);
     } else {
       entries = generateDomTree('tr', {}, [
         generateDomTree('td', { textContent: 'No time' }),
         generateDomTree('td', { textContent: 'No plans made yet' })
       ]);
-
+      $tableSelector.appendChild(entries);
     }
-    $tableSelector.appendChild(entries);
   }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // Data Model
