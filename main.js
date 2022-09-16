b7e55bc4055800ea49f173b589b6f1c64d2cc917
Daisy Yoo <daisyhyoo@gmail.com>
2022-09-16 21:36:20 +0000

    Data model in progress

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var $modal = document.querySelector('.modal-background-container');
 var $addEntryButton = document.querySelector('.new-entry-button');
 var $weekDaySpacingDiv = document.querySelector('.weekday-spacing');
 var $tableHeader = document.querySelector('.table-header');
-var $tableSelector = document.querySelector('table');
+var $tableSelector = document.querySelector('tbody');
 
 // event listeners
 $modal.addEventListener('submit', handleModalClick);
@@ -22,8 +22,6 @@ function handleModalClick(event) {
 
 function scheduleUpdate(event) {
   $tableHeader.textContent = 'Scheduled Events for ' + event.target.textContent;
-  console.log(event.target.textContent.toLowerCase());
-  // debugger;
   createListEntries(data, event.target.textContent.toLowerCase());
 }
 
@@ -46,21 +44,34 @@ function generateDomTree(tagName, attributes, children = []) {
 // Making a DOM Tree
 
 function createListEntries(object, dayOfWeek) {
-  var entries = generateDomTree('tbody', {}, [
-    generateDomTree('tr', {}, [
-      generateDomTree('td', { textContent: object[dayOfWeek][0].time }),
-      generateDomTree('td', { textContent: object[dayOfWeek][0].description })
-    ])
-  ]);
-  $tableSelector.appendChild(entries);
+  for (var i = 0; i < object[dayOfWeek].length; i++) {
+    if (object[dayOfWeek][i].time) {
+      var entries = generateDomTree('tr', {}, [
+        generateDomTree('td', { textContent: object[dayOfWeek][i].time }),
+        generateDomTree('td', { textContent: object[dayOfWeek][i].description })
+      ]);
+      $tableSelector.appendChild(entries);
+    } else {
+      var emptyEntry = generateDomTree('tr', {}, [
+        generateDomTree('td', { textContent: 'No time' }),
+        generateDomTree('td', { textContent: 'No plans made yet' })
+      ]);
+      $tableSelector.appendChild(emptyEntry);
+    }
+  }
 }
 
 // Data Model
+
 var data = {
   monday: [
     {
       time: 'Some time',
       description: 'Yes'
+    },
+    {
+      time: '12:00',
+      description: 'SURE?'
     }
   ],
   tuesday: [
@@ -99,4 +110,9 @@ var data = {
       description: ''
     }
   ]
+};
+
+var dataModel = {
+  days: data,
+  view: 'monday'
 };
