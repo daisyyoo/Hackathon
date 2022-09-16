d2322f034081963e8fe898f9f7953d4361c88731
Anthony Urbina <aurbinava002@gmail.com>
2022-09-16 19:31:44 +0000

    table header changes to reflect new day that was clicked

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+// variable definitions
 var $modal = document.querySelector('.modal-background-container');
 var $addEntryButton = document.querySelector('.new-entry-button');
+var $weekDaySpacingDiv = document.querySelector('.weekday-spacing');
+var $tableHeader = document.querySelector('.table-header');
+
+// event listeners
 $modal.addEventListener('submit', handleModalClick);
 $addEntryButton.addEventListener('click', addEntryButton);
+$weekDaySpacingDiv.addEventListener('click', scheduleUpdate);
 
+// event listener functions
 function addEntryButton(event) {
   $modal.className = 'modal-background-container container';
 }
@@ -10,4 +17,25 @@ function addEntryButton(event) {
 function handleModalClick(event) {
   event.preventDefault();
   $modal.className = 'hidden';
+}
+
+function scheduleUpdate(event) {
+  $tableHeader.textContent = 'Scheduled Events for ' + event.target.textContent;
+
+}
+
+// other functions
+function generateDomTree(tagName, attributes, children = []) {
+  var element = document.createElement(tagName);
+  for (var key in attributes) {
+    if (key === 'textContent') {
+      element.textContent = attributes.textContent;
+    } else {
+      element.setAttribute(key, attributes[key]);
+    }
+  }
+  for (var i = 0; i < children.length; i++) {
+    element.append(children[i]);
+  }
+  return element;
 }
